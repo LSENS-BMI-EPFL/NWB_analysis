023eefa660dddd24831e369873ab86fa0ce81519
Robin Dard <robin.dard@epfl.ch>
2024-02-18 22:55:36 +0100

    update in WF analysis plots

--- analysis/widefield_analysis.py
+++ analysis/widefield_analysis.py
@@ -79,7 +79,7 @@ def return_events_aligned_wf_table(nwb_files, rrs_keys, trials_dict, trial_names
     for index, trial_dict in enumerate(trials_dict):
         for epoch_index, epoch in enumerate(epochs):
             print(f" ")
-            print(f"Trial selection : {trials_dict[index]}")
+            print(f"Trial selection : {trials_dict[index]} (Trial name : {trial_names[index]})")
             print(f"Epoch : {epoch}")
             data_table = return_events_aligned_data_table(nwb_list=nwb_files,
                                                           rrs_keys=rrs_keys,
@@ -95,7 +95,7 @@ def return_events_aligned_wf_table(nwb_files, rrs_keys, trials_dict, trial_names
 
 
 if __name__ == "__main__":
-    experimenter_initials = "PB"
+    experimenter_initials = "RD"
 
     root_path = server_path.get_experimenter_nwb_folder(experimenter_initials)
 
@@ -104,10 +104,36 @@ if __name__ == "__main__":
     all_nwb_names = os.listdir(root_path)
 
     subject_ids = ['RD039']
-    # plots_to_do = ['single_session']
-    # session_to_do = ["RD039_20240208_143129", "RD039_20240209_162220", "RD039_20240210_140338",
-    #                  "RD039_20240211_142239", "RD039_20240207_151646"]
-    session_to_do = ["RD039_20240211_142239"]
+
+    # Sessions
+    # session_to_do = [
+    #
+    #     "RD039_20240208_143129", "RD039_20240209_162220",
+    #     "RD039_20240210_140338"
+    # ]
+
+    # Selection of sessions with no wf frames missing
+    # session_to_do = [
+    #     "RD039_20240124_142334", "RD039_20240125_142517",
+    #     "RD039_20240206_134324",
+    #     "RD039_20240208_143129", "RD039_20240209_162220",
+    #     "RD039_20240210_140338", "RD039_20240212_135702",
+    #     "RD039_20240213_161938", "RD039_20240214_164330",
+    #     "RD039_20240215_142858"
+    # ]
+
+    # Selection of sessions with no WF frames missing and 'good' behavior
+    session_to_do = [
+        "RD039_20240124_142334", "RD039_20240125_142517",
+
+        "RD039_20240209_162220",
+        "RD039_20240210_140338", "RD039_20240212_135702",
+        "RD039_20240213_161938",
+        "RD039_20240215_142858"
+    ]
+
+    # To do single session
+    # session_to_do = ["RD039_20240124_142334"]
 
     for subject_id in subject_ids:
         nwb_names = [name for name in all_nwb_names if subject_id in name]
@@ -119,6 +145,7 @@ if __name__ == "__main__":
 
         # plot_wf_activity(nwb_files, output_path)
 
+        # Create dataframe of traces aligned to events
         trials_dict = [{'whisker_stim': [1], 'lick_flag': [1]},
                        {'whisker_stim': [1], 'lick_flag': [0]},
                        {'auditory_stim': [1], 'lick_flag': [1]},
@@ -131,7 +158,7 @@ if __name__ == "__main__":
 
         epochs = ['rewarded', 'non-rewarded']
 
-        t_range = (2, 1.5)
+        t_range = (0.8, 1.5)
 
         df = return_events_aligned_wf_table(nwb_files=nwb_files,
                                             rrs_keys=['ophys', 'brain_area_fluorescence', 'dff0_traces'],
@@ -140,18 +167,114 @@ if __name__ == "__main__":
                                             epochs=epochs,
                                             time_range=t_range)
 
-        fig = plt.subplots()
-        # group
-        data_to_plot = df.groupby(["mouse_id", "session_id", "trial_type", "epoch",
-                                   "behavior_day", "behavior_type", "roi", "cell_type", "time"], as_index=False).\
-            agg(np.nanmean)
+        # Group data by sessions and mice
+        avg_data_to_plot = df.groupby(["mouse_id", "session_id", "trial_type", "epoch",
+                                      "behavior_day", "behavior_type", "roi", "cell_type", "time"],
+                                      as_index=False).agg(np.nanmean)
 
-        # plots
-        # data_to_plot = data_to_plot.loc[(df.trial_type.isin(['whisker_hit']))]
-        # sns.lineplot(data=data_to_plot, x='time', y='activity', hue='cell_type')
+        # Plot from average (one point per time per session) in the two contexts
+        # Plot all area to see successive activation
+        fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+        rwd_data_to_plot = avg_data_to_plot.loc[(avg_data_to_plot.trial_type.isin(['whisker_hit'])) &
+                                                (avg_data_to_plot.cell_type.isin(
+                                                    ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                                (avg_data_to_plot.epoch == 'rewarded')]
+        sns.lineplot(data=rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[0, 0])
+        rwd_data_to_plot = avg_data_to_plot.loc[(avg_data_to_plot.trial_type.isin(['whisker_hit'])) &
+                                                (avg_data_to_plot.cell_type.isin(
+                                                    ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                                (avg_data_to_plot.epoch == 'non-rewarded')]
+        sns.lineplot(data=rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[1, 0])
+        axs[0, 0].set_title('Whisker hit Rewarded context')
+        axs[1, 0].set_title('Whisker hit Non rewarded context')
 
-        data_to_plot = data_to_plot.loc[(data_to_plot.cell_type == 'A1') &
-                                        (data_to_plot.trial_type.isin(['whisker_hit', 'whisker_miss']))]
-        sns.lineplot(data=data_to_plot, x='time', y='activity', hue='epoch', style='trial_type')
+        nn_rwd_data_to_plot = avg_data_to_plot.loc[(avg_data_to_plot.trial_type.isin(['whisker_miss'])) &
+                                                   (avg_data_to_plot.cell_type.isin(
+                                                    ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                                   (avg_data_to_plot.epoch == 'rewarded')]
+        sns.lineplot(data=nn_rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[0, 1])
+        nn_rwd_data_to_plot = avg_data_to_plot.loc[(avg_data_to_plot.trial_type.isin(['whisker_miss'])) &
+                                                   (avg_data_to_plot.cell_type.isin(
+                                                    ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                                   (avg_data_to_plot.epoch == 'non-rewarded')]
+        sns.lineplot(data=nn_rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[1, 1])
+        axs[0, 1].set_title('Whisker miss Rewarded context')
+        axs[1, 1].set_title('Whisker miss Non rewarded context')
+        # plt.show()
+        saving_folder = os.path.join(output_path, "RD039")
+        if not os.path.exists(saving_folder):
+            os.makedirs(saving_folder)
+        fig.savefig(os.path.join(saving_folder, 'average.png'))
 
-        plt.show()
+        # Plot per area to compare the two contexts in each:
+        areas = ['A1', 'wS1', 'wS2', 'wM1', 'wM2', 'tjM1']
+        for area in areas:
+            fig, ax = plt.subplots(1, 1)
+            data_to_plot = avg_data_to_plot.loc[(avg_data_to_plot.cell_type == area) &
+                                                (avg_data_to_plot.trial_type.isin(['whisker_hit', 'whisker_miss']))]
+            sns.lineplot(data=data_to_plot, x='time', y='activity', hue='epoch', style='trial_type', ax=ax)
+            plt.suptitle(f"{area} response to whisker trials")
+            # plt.show()
+            saving_folder = os.path.join(output_path, "RD039")
+            if not os.path.exists(saving_folder):
+                os.makedirs(saving_folder)
+            fig.savefig(os.path.join(saving_folder, 'average_by_area.png'))
+
+        # Plot with single session
+        # Plot with all area
+        for session in session_to_do:
+            fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+            rwd_data_to_plot = df.loc[(df.trial_type.isin(['whisker_hit'])) &
+                                      (df.cell_type.isin(
+                                                        ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                      (df.epoch == 'rewarded') &
+                                      (df.session_id == session)]
+            sns.lineplot(data=rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[0, 0])
+            rwd_data_to_plot = df.loc[(df.trial_type.isin(['whisker_hit'])) &
+                                      (df.cell_type.isin(
+                                                        ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                      (df.epoch == 'non-rewarded') &
+                                      (df.session_id == session)]
+            sns.lineplot(data=rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[1, 0])
+            axs[0, 0].set_title('Whisker hit Rewarded context')
+            axs[1, 0].set_title('Whisker hit Non rewarded context')
+
+            nn_rwd_data_to_plot = df.loc[(df.trial_type.isin(['whisker_miss'])) &
+                                         (df.cell_type.isin(
+                                                           ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                         (df.epoch == 'rewarded') &
+                                         (df.session_id == session)]
+            sns.lineplot(data=nn_rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[0, 1])
+            nn_rwd_data_to_plot = df.loc[(df.trial_type.isin(['whisker_miss'])) &
+                                         (df.cell_type.isin(
+                                                           ['tjS1', 'wS1', 'wS2', 'tjM1', 'wM1', 'wM2'])) &
+                                         (df.epoch == 'non-rewarded') &
+                                         (df.session_id == session)]
+            sns.lineplot(data=nn_rwd_data_to_plot, x='time', y='activity', hue='cell_type', ax=axs[1, 1])
+            axs[0, 1].set_title('Whisker miss Rewarded context')
+            axs[1, 1].set_title('Whisker miss Non rewarded context')
+            # plt.show()
+            saving_folder = os.path.join(output_path, f"{session[0:5]}", f"{session}")
+            if not os.path.exists(saving_folder):
+                os.makedirs(saving_folder)
+            fig.savefig(os.path.join(saving_folder, f"whisker.png"))
+            plt.close()
+
+        # Plot by area
+        areas = ['A1', 'wS1', 'wS2', 'wM1', 'wM2', 'tjM1']
+        for session in session_to_do:
+            for area in areas:
+                sub_data_to_plot = df.loc[(df.cell_type == area) &
+                                          (df.trial_type.isin(['whisker_hit', 'whisker_miss'])) &
+                                          (df.session_id == session)]
+                fig, ax = plt.subplots(1, 1)
+                sns.lineplot(data=sub_data_to_plot, x='time', y='activity', hue='epoch', style='trial_type', ax=ax)
+
+                plt.suptitle(f"{session[0:5]}, {session}, {area} response to whisker trials")
+
+                # plt.show()
+                saving_folder = os.path.join(output_path, f"{session[0:5]}", f"{session}")
+                if not os.path.exists(saving_folder):
+                    os.makedirs(saving_folder)
+                fig.savefig(os.path.join(saving_folder, f"{area}.png"))
+                plt.close()
