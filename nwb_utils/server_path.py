b80a65aae0fd14ded84336ae69a0cc0869848ef3
Robin Dard <robin.dard@epfl.ch>
2024-05-20 10:11:40 +0200

    Add small function to convert list of session id to list of nwb path

--- nwb_utils/server_path.py
+++ nwb_utils/server_path.py
@@ -27,3 +27,20 @@ def get_experimenter_saving_folder_root(experimenter_initials):
 
     return saving_folder
 
+
+def from_session_list_to_path_list(sessions_list):
+    sessions_path = []
+    for session_id in sessions_list:
+        experimenter = session_id[0:2]
+        nwb_folder = get_experimenter_nwb_folder(experimenter)
+        files = os.listdir(nwb_folder)
+        nwb_files = [os.path.join(nwb_folder, name) for name in files if 'nwb' in name]
+        for nwb_path in nwb_files:
+            if session_id in nwb_path:
+                sessions_path.append(nwb_path)
+                break
+            else:
+                continue
+
+    return sessions_path
+
