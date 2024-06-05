45fc3cab1c7b2889a21b907a2fee1477cb9e59b4
aprrenard <aprrenard@protonmail.com>
2024-06-05 14:37:09 +0200

    Filter events with list of trials.

--- nwb_utils/utils_two_photons.py
+++ nwb_utils/utils_two_photons.py
@@ -16,13 +16,12 @@ def center_rrs_on_events(traces, traces_ts, event_ts, time_range, sampling_rate,
     n_tp = int(time_range[0] + time_range[1] + 1)
     activity_aligned = np.zeros((n_cells, len(event_frames), n_tp)) * np.nan
     for idx, frame in enumerate(event_frames):
-        if frame+time_range[1]+1 > traces.shape[1]:   # Remove event if to close to the end
+        if frame+time_range[1]+1 > traces.shape[1]:   # Remove event if too close to the end
             continue
         activity_aligned[:, idx] = traces[:, frame-time_range[0]:frame+time_range[1]+1]
 
     if subtract_baseline:
-        baseline = np.nanmean(activity_aligned[:, :, 0:time_range[0]], axis=2)
-        activity_aligned = activity_aligned - baseline[:, :, None]
+        activity_aligned = activity_aligned - np.nanmean(activity_aligned[:, :, 0:time_range[0]], axis=2, keepdims=True)
 
     return activity_aligned
 
@@ -35,8 +34,10 @@ def select_activity_around_events_pd(activity, activity_ts, rois, events, time_r
                                             events, time_range,
                                             sampling_rate, subtract_baseline)
     n_cells, n_events, n_t = activity_aligned.shape
+
     time_stamps_vect = np.linspace(-time_range[0], time_range[1], n_t)
     time_stamps_vect = np.tile(time_stamps_vect, n_cells * n_events)
+
     event_vect = np.tile(np.repeat(np.arange(n_events), n_t), n_cells)
     rois_vect = np.repeat(rois, n_events * n_t)
     activity_reshaped = activity_aligned.flatten()
