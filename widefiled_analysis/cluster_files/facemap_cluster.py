da676d53ebdbc450779ac72bdd947d7480bb4160
Robin Dard <robin.dard@epfl.ch>
2025-03-04 13:55:18 +0100

    facemap cluster files

--- widefiled_analysis/cluster_files/facemap_cluster.py
+++ widefiled_analysis/cluster_files/facemap_cluster.py
@@ -1,6 +1,12 @@
+import sys
 from facemap import process
-
 
 def main(vid_path, proc_path):
     process.run(vid_path, proc=proc_path)
 
+
+if __name__ == '__main__':
+    vid = sys.argv[1]
+    proc = sys.argv[2]
+
+    main(vid, proc)
