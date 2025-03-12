28bbb5878da85d4c962b2ed526a2db1783ef397b
Robin Dard <robin.dard@epfl.ch>
2025-03-12 15:55:31 +0100

    facemap retrieve results

---
+++ widefiled_analysis/cluster_files/transfer_facemap_results.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import json
+import shutil
+
+
+def transfer_results():
+
+    result_folder = f"/scratch/rdard/facemap/results"
+    dest_folder = f"/home/rdard/servers"
+    vid_folder = f"/scratch/rdard/facemap/videos"
+
+    for result in os.listdir(result_folder):
+
+        session_id = "_".join(result.split('_')[0:3])
+
+        facemap_path = os.path.join(result_folder, result)
+        vid_path = os.path.join(vid_folder, f'{session_id}_sideview.avi')
+
+        # if os.path.exists(vid_path):
+        #     os.remove(vid_path)
+
+        output_folder = os.path.join(dest_folder, "analysis", "Robin_Dard", 'facemap_cluster_results')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        print(f"Copying data from: {facemap_path} to: {output_folder}")
+
+        shutil.copyfile(facemap_path, output_folder)
+
+    return
+
+
+if __name__ == "__main__":
+    transfer_results()
